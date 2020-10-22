acc0d9b41ef1a1c85548f0f0eb8552617d1bbf30
Hung Truong <hungtruongquoc@gmail.com>
2020-10-21 20:25:14 -0500

    20201021: Added ngrx/store to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,16 @@ import { CoreModule } from './@core/core.module';
 import { LayoutModule } from './@ui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,8 +29,13 @@ import { AppComponent } from './app.component';
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
     }),
+    FormsModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
